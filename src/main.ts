3029dff94cddb6f4817e74507c311c337c95ac70
Yusuke Kawamoto <novogrammer@gmail.com>
2024-06-27 16:48:34 +0900

    青森県も追加

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,25 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 
 async function mainAsync(){
-  const xmlData = await fetch("./P34-14_01.xml").then((res)=>res.text());
-  const parser = new XMLParser({
-    ignoreAttributes:false,
-  });
-  const obj=parser.parse(xmlData);
-  // console.log(obj);
-  const points:LatLng[]=obj?.["ksj:Dataset"]?.["gml:Point"]?.map((element:any)=>element?.["gml:pos"]).filter((point?:string)=>!!point).map((point:string)=>{
-    const [lat,lng]=point.split(" ");
-    return {
-      lat:parseFloat(lat),
-      lng:parseFloat(lng),
-    }
-  });
+  const points=[];
+  const gmlUrlList=["./P34-14_01.xml","./P34-14_02.xml"];
+  for(let gmlUrl of gmlUrlList){
+    const xmlData = await fetch(gmlUrl).then((res)=>res.text());
+    const parser = new XMLParser({
+      ignoreAttributes:false,
+    });
+    const obj=parser.parse(xmlData);
+    // console.log(obj);
+    const subpoints:LatLng[]=obj?.["ksj:Dataset"]?.["gml:Point"]?.map((element:any)=>element?.["gml:pos"]).filter((point?:string)=>!!point).map((point:string)=>{
+      const [lat,lng]=point.split(" ");
+      return {
+        lat:parseFloat(lat),
+        lng:parseFloat(lng),
+      }
+    });
+    points.push(...subpoints);
+  
+  }
   // console.log(points);
   (window as any).app=new App(points);
 
